f49a58b8c8814e53648a61a0ed709b04400bbde8
Ugozeal <okonkwodavidu@gmail.com>
2021-03-11 17:09:25 +0100

    created the pod file and installed the required dependency and started the project

---
+++ Jokes_App_with_Alamofire/Jokes_App_with_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokes_App_with_AlamofireApp.swift
+//  Jokes_App_with_Alamofire
+//
+//  Created by David U. Okonkwo on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Jokes_App_with_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '13.0'
+
+target 'Jokes_App_with_Alamofire' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Jokes_App_with_Alamofire
+	pod 'Alamofire', '= 5.0.0-rc.2'
+
+end
